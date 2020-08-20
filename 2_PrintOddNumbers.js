0c6c634250d6aae192b2ac82e5b9817379e9b1df
AhmetBasari <AhmetBasari@users.noreply.github.com>
2020-08-20 12:49:29 +0200

    git@github.com:AhmetBasari/Argorithms-bsc-js.git

---
+++ 2_PrintOddNumbers.js
@@ -0,0 +1,11 @@
+// We need to write from 1 to 135
+// if anynumber divide to two then the result that is 1 
+// look with output
+
+for (var i=1; i<=135; i++){
+    if (i % 2 == 1){
+        console.log(i)
+    }
+    else if  (i%3 ==0)
+    console.log(i) 
+}
---
+++ 4_Printtheelementsofanarray.js
@@ -0,0 +1,10 @@
+// we need to write all length of array 
+
+function given_all(arr){
+    for (var i=0; i< arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+
+var n=[1, 4, 2, 12]
+given_all(n);
