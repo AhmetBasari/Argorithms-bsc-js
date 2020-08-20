c5509fe685334b463fa61afa03fb1f601ce590ee
AhmetBasari <AhmetBasari@users.noreply.github.com>
2020-08-20 10:32:37 +0200

    2.commit

---
+++ PrintArray.js
@@ -0,0 +1,13 @@
+// write a function that takes array as an argument 
+// For loop from 0 the length of the array (the argument)
+// print the value
+
+function print_Array(arr){
+    for( var i=0; i< arr.length; i++){
+        console.log(arr[i])
+    }
+}
+var X=[1,4,2,12]
+print_Array(X)
+var test = [2, 15, 'Rotterdam', 25, true, 0]
+print_Array(test)
