f90d8042a9193a400ecdf7ee7ec466ef06e2050e
AhmetBasari <AhmetBasari@users.noreply.github.com>
2020-08-20 10:38:41 +0200

    commit for array

--- PrintArray.js
+++ PrintArray.js
@@ -11,3 +11,9 @@ var X=[1,4,2,12]
 print_Array(X)
 var test = [2, 15, 'Rotterdam', 25, true, 0]
 print_Array(test)
+
+// i=0 => 1 ... arr[0] = 1
+// i=1 => 4 ... arr[1] = 4
+// i=2 => 2 ... arr[2] = 2
+// i=3 => 12 ... arr[3] = 12
+// i=4 => the for loop stops
